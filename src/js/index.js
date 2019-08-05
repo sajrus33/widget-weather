816a14013ca0bace318c5c466b672064c876ceff
sajrus33 <brianwala22@gmail.com>
2019-08-05 14:04:31 +0200

    media 1/2 done

--- src/js/index.js
+++ src/js/index.js
@@ -194,10 +194,6 @@ const widget = {
     */
 
     sortData: data => {
-        // console.log(data);
-        // data.forEach(location => {
-        //     console.log(location)
-        // })
 
         const dataSorted = [];
         const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -235,44 +231,3 @@ addEventListener("load", widget.init);
 
 
 
-
-
-
-
-
-
-// class DOMelement {
-//     constructor(options) {
-//         this.type = options.type;
-//         this.txt = options.txt;
-//         this.clas = options.clas;
-//         this.parent = options.parent;
-//         this.src = options.src;
-//     }
-
-//     const self = document.createElement(this.type);
-//     /         addClass(this[name], clas);
-//     //         this[name].innerText = text;
-//     //         parent.append(this[name]);
-// }
-
-
-// const createDOMElement = (
-//     options,
-//     text = "5",
-// ) => {
-//     const elementDOM = document.createElement(options.type);
-//     elementDOM.classList.add(options.clas);
-//     elementDOM.innerText = options.txt;
-//     options.parent.append(elementDOM);
-// };
-
-
-
-// const own = createDOMElement({
-//     type: "div",
-//     txt: "polluted",
-//     parent: document.body,
-//     clas: "widget__today"
-// })
-// console.log(own);
